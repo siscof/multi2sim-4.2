0f94428209f93a8fde477cabfee07d363866c492
siscof <sisco_f@hotmail.com>
2016-08-12 10:05:07 +0200

    la condicion para entrar sin permisos al sistema de memoria es que la cantidad de mshr usados sea menos de la mitad de este

--- src/arch/southern-islands/timing/vector-mem-unit.c
+++ src/arch/southern-islands/timing/vector-mem-unit.c
@@ -241,7 +241,7 @@ void si_vector_mem_mem(struct si_vector_mem_unit_t *vector_mem)
 			}
 			if(!wavefront_permitido)
 			{
-				if(vector_mem->compute_unit->vector_cache->mshr->entradasOcupadas > (vector_mem->compute_unit->vector_cache->mshr->size - 4))
+				if(vector_mem->compute_unit->vector_cache->mshr->entradasOcupadas > (vector_mem->compute_unit->vector_cache->mshr->size /2))
 				{
 					//uop_in_mem_buffer = list_head(vector_mem->mem_buffer);
 					//if(!(uop_in_mem_buffer->wavefront->wavefront_pool_entry->wait_for_mem))
